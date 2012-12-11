37e1cec69c0ba28a417b1f89793bcf217e04b0b7
Eloy Gomez <eloy@indeos.es>
2012-12-11 22:07:16 +0100

    Add spec if user provide user but not password

--- spec/request/api_key/api_key_spec.rb
+++ spec/request/api_key/api_key_spec.rb
@@ -3,11 +3,17 @@ require 'spec_helper'
 describe '/api_key' do
 
   it 'should return 401 with invalid credentials' do
-    create_user 'test@debox.com'
     get '/v1/api_key'
     last_response.should_not be_ok
     last_response.status.should eq 401
   end
+
+  it 'should return 401 with invalid credentials' do
+    get '/v1/api_key', user: 'test@debox.com'
+    last_response.should_not be_ok
+    last_response.status.should eq 401
+  end
+
 
   it 'should return token with valid credentials' do
     user = create_user 'test@debox.com'
