91c13d54a6745f7555a914de78342315dfa3509b
Eloy Gomez <eloy@indeos.es>
2013-01-13 17:50:44 +0100

    Add acl_add

--- lib/debox_server/acl.rb
+++ lib/debox_server/acl.rb
@@ -9,7 +9,16 @@ module DeboxServer
     # Return nil if not found
     def acl_find(app, env, user)
       acl = redis.hget acl_key_name(app, env), user
-      JSON.parse acl if acl
+      JSON.parse(acl).map(&:to_sym) if acl
+    end
+
+    # Add an action to the app acl for the given user
+    def acl_add(app, env, user, action)
+      acl = acl_find(app, env, user) || []
+      unless acl.include? action
+        acl << action
+        redis.hset acl_key_name(app, env), user, acl.to_json
+      end
     end
 
     # Return the app acl key nane
--- spec/unit/acl_spec.rb
+++ spec/unit/acl_spec.rb
@@ -4,14 +4,27 @@ describe DeboxServer::ACL do
   describe 'acl_find' do
     it 'should find the app acl for the given user' do
       email = 'test@indeos.es'
-      DeboxServer::RedisDB::redis.hset server.acl_key_name('app', 'env'), email, ['*', 'cap'].to_json
-      server.acl_find('app', 'env', email).should eq ['*', 'cap']
+      DeboxServer::RedisDB::redis.hset server.acl_key_name('app', 'env'), email, [:*, :cap].to_json
+      server.acl_find('app', 'env', email).should eq [:*, :cap]
     end
 
     it 'should return nil if not found' do
       email = 'test@indeos.es'
       server.acl_find('app', 'env', email).should be_nil
     end
+  end
+
+  describe 'acl_add' do
+    it 'should create an acl with the given action' do
+      server.acl_add 'app', 'env', 'test@indeos.es', :cap
+      server.acl_find('app', 'env', 'test@indeos.es').should eq [:cap]
+    end
+    it 'should add actions to the current acl' do
+      server.acl_add 'app', 'env', 'test@indeos.es', :cap
+      server.acl_add 'app', 'env', 'test@indeos.es', :recipes
+      server.acl_find('app', 'env', 'test@indeos.es').should eq [:cap, :recipes]
+    end
 
   end
+
 end
