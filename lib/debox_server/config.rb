4cb3650ddfcf9705c54c811fefee24129d933071
Eloy Gomez <eloy@indeos.es>
2013-01-25 20:11:43 +0100

    Add method for get db configuration

--- lib/debox_server/config.rb
+++ lib/debox_server/config.rb
@@ -2,7 +2,20 @@ module DeboxServer
   module Config
 
     def config_dir
+      Config::config_dir
+    end
+
+    def self.config_dir
       ENV['DEBOX_SERVER_CONFIG'] || File.join(DEBOX_ROOT, 'config')
+    end
+
+    def db_conf
+      Config::db_conf
+    end
+
+    def self.db_conf
+      db_conf_file = File.join Config::config_dir, 'database.yml'
+      YAML.load(File.read(db_conf_file))
     end
 
   end
