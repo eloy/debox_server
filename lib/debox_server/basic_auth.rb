70af25b5da02f81124730d2c801f1851ba030fcb
Eloy Gomez <eloy@indeos.es>
2013-01-26 01:04:49 +0100

    Add require_auth helper

--- lib/debox_server/basic_auth.rb
+++ lib/debox_server/basic_auth.rb
@@ -27,5 +27,14 @@ module DeboxServer
       error!("Forbidden", 403) unless current_user.admin
     end
 
+    def require_auth_for(action, opt = { })
+      app = opt[:app] || current_app
+      env = opt[:env] || current_env
+      user = opt[:user] || current_user
+      unless acl_allow? app, env, user, action
+        error!("Forbidden", 403)
+      end
+    end
+
   end
 end
