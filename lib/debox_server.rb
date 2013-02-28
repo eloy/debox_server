a99fbd15421260208c5a2f8ddb95583b14c4d4ab
Eloy Gomez <eloy@indeos.es>
2013-02-28 22:28:23 +0100

    Re include activerecord

--- lib/debox_server.rb
+++ lib/debox_server.rb
@@ -15,7 +15,7 @@ require "debox_server/logger"
 require "debox_server/config"
 require "debox_server/utils"
 require 'debox_server/redis'
-# require 'debox_server/activerecord'
+require 'debox_server/activerecord'
 require "debox_server/ssh_keys"
 require "debox_server/apps"
 require "debox_server/users"
@@ -25,10 +25,6 @@ require "debox_server/job"
 require "debox_server/job_notifier"
 require "debox_server/jobs_queue"
 require "debox_server/acl"
-
-
-# TODO get root without the ../
-DEBOX_ROOT = File.join(File.dirname(__FILE__), '../')
 
 module DeboxServer
   include DeboxServer::Logger
