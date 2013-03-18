56ac6f0b14665b8343ed0f4ce5b839a715aa1c0f
Eloy Gomez <eloy@indeos.es>
2013-03-18 19:04:18 +0100

    Remove unused code

--- lib/debox_server/output_multiplexer.rb
+++ lib/debox_server/output_multiplexer.rb
@@ -1,39 +1,8 @@
 module DeboxServer
-  # module DeployLogs
-
-  #   MAX_LOGS_COUNT = 15
-
-  #   def deployer_logs(app, env)
-  #     range = redis.lrange log_key_name(app, env), 0, MAX_LOGS_COUNT
-  #     range.map do |log|
-  #       JSON.parse log, symbolize_names: true
-  #     end
-  #   end
-
-  #   def deployer_logs_last(app, env)
-  #     deployer_logs_at app, env, 0
-  #   end
-
-  #   def deployer_logs_at(app, env, index)
-  #     log = redis.lindex(log_key_name(app, env), index)
-  #     JSON.parse log, symbolize_names: true if log
-  #   end
-
-  #   def deployer_logs_count(app, env)
-  #     redis.llen log_key_name(app, env)
-  #   end
-
-  #   def deployer_logs_destroy(app, env)
-  #     redis.del log_key_name(app, env)
-  #   end
-
-  #   def log_key_name(app, env)
-  #     "logger_#{app}_#{env}"
-  #   end
-  # end
-
 
   # Output multiplexer
+  #
+  # Used as output for capistrano
   class OutputMultiplexer
     attr_reader :buffer, :time, :result, :success, :error
 
