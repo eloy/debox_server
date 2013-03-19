4a5c7d45af6055db1ebebd3ed0f56457305bc1b4
Eloy Gomez <eloy@indeos.es>
2013-03-19 18:37:27 +0100

    Refactor notifier method creation

--- lib/debox_server/job_notifier.rb
+++ lib/debox_server/job_notifier.rb
@@ -1,23 +1,31 @@
 module DeboxServer
   class JobNotifier
     include DeboxServer::Logger
+
     def channel
       @channel ||= EM::Channel.new
     end
 
     private
 
-    def method_missing(method_name, *args)
-      job = args[0]
-      opt = args[1] || { }
-      send_notification method_name, job, opt
-    end
-
     def send_notification(notification, job, opt={ })
+      log.info "Notification: #{notification}"
       msg = {notification: notification, job: job}
       msg.merge! opt
       channel.push msg.to_json
     end
+
+    def self.notifications(*args)
+      args.each do |notification|
+        define_method notification do |*args|
+          job = args[0]
+          opt = args[1] || { }
+          send_notification notification, job, opt
+        end
+      end
+    end
+
+    notifications :started, :stdout, :added
 
   end
 end
