91585c6a2a13b66339070c15c09732444dcf1d70
Eloy Gomez <eloy@indeos.es>
2013-06-19 20:50:38 +0200

    Do not include objet root when serialize to json

--- lib/debox_server/activerecord.rb
+++ lib/debox_server/activerecord.rb
@@ -1,12 +1,14 @@
 require 'active_record'
 
 module DeboxServer
-
   ActiveRecord::Base.configurations = Config::db_conf
   current_env = (ENV['RACK_ENV'] || 'development')
   # drop and create need to be performed with a connection to the 'postgres' (system) database
   ActiveRecord::Base.establish_connection Config::db_conf[current_env]
 
+
+  # Setup defaults
+  ActiveRecord::Base.include_root_in_json = false
 end
 
 # Require models
